aa62f0705e710ac74f47a78b6d65e4735f0bddcb
Sven Hergenhahn <sven.hergenhahn@dm.de>
2022-05-31 15:51:35 +0200

    migration nvim_tree: see https://github.com/kyazdani42/nvim-tree.lua/issues/674

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,75 +1,7 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
-
 
 -- TEMPORARY HACK
-vim.g["nvim_tree_git_hl"] = 1 --0 by default, will enable file highlight for git attributes (can be used without the icons).
-vim.g["nvim_tree_highlight_opened_files"] = 1 --0 by default, will enable folder and file icon highlight for opened files/directories.
-vim.g["nvim_tree_root_folder_modifier"] = ':~' --This is the default. See :help filename-modifiers for more options
-vim.g["nvim_tree_add_trailing"] = 1 --0 by default, append a trailing slash to folder names
-vim.g["nvim_tree_group_empty"] = 1 -- 0 by default, compact folders that only contain a single folder into one node in the file tree
-vim.g["nvim_tree_icon_padding"] = ' ' --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-vim.g["nvim_tree_symlink_arrow"] = ' >> ' -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
-vim.g["nvim_tree_respect_buf_cwd"] = 1 --0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
-vim.g["nvim_tree_create_in_closed_folder"] = 1 --0 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
-vim.g["nvim_tree_special_files"] = { ["README.md"] = 1, Makefile = 1, MAKEFILE = 1 } -- List of filenames that gets highlighted with NvimTreeSpecialFile
-vim.g["nvim_tree_show_icons"] = {
-      git = 1,
-      folders = 1,
-      files = 1,
-      folder_arrows = 1,
-    }
---If 0, do not show the icons for one of 'git' 'folder' and 'files'
---1 by default, notice that if 'files' is 1, it will only display
---if nvim-web-devicons is installed and on your runtimepath.
---if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
---but this will not work when you set renderer.indent_markers.enable (because of UI conflict)
-
--- default will show icon by default if no icon is provided
--- default shows no icon by default
-vim.g["nvim_tree_icons"] = {
-          default = "",
-          symlink = "",
-          git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌"
-        },
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        }
-      }
 
 vim.cmd "nnoremap <C-n> :NvimTreeToggle<CR>"
 vim.cmd "nnoremap <leader>r :NvimTreeRefresh<CR>"
@@ -107,6 +39,8 @@ nvim_tree.setup {
   disable_netrw = true,
   hijack_netrw = true,
   open_on_setup = false,
+  create_in_closed_folder = true,    --false by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
+  respect_buf_cwd = true,             --false by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
   ignore_ft_on_setup = {
     "startify",
     "dashboard",
@@ -168,6 +102,57 @@ nvim_tree.setup {
     cmd = "trash",
     require_confirm = true,
   },
+  renderer = {
+    icons = {
+      symlink_arrow = ' >> ', -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
+      show = {
+        git = true,
+        folder = true,
+        file = true,
+        folder_arrow = true,
+      },
+      padding = ' ',        --one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
+      glyphs = {
+          default = "",
+          symlink = "",
+          git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌"
+        },
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+      },
+    },
+    special_files = {
+      ["README.md"] = 1, Makefile = 1, MAKEFILE = 1 
+    },                               -- List of filenames that gets highlighted with NvimTreeSpecialFile
+    add_trailing = true,
+    group_empty = true,
+    highlight_git = true,            -- false by default, will enable file highlight for git attributes (can be used without the icons).
+    highlight_opened_files = "icon", -- 0 by default, will enable folder and file icon highlight for opened files/directories.
+    root_folder_modifier = ':~',     -- This is the default. See :help filename-modifiers for more options
+  },
+--If 0, do not show the icons for one of 'git' 'folder' and 'files'
+--1 by default, notice that if 'files' is 1, it will only display
+--if nvim-web-devicons is installed and on your runtimepath.
+--if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
+--but this will not work when you set renderer.indent_markers.enable (because of UI conflict)
+
+-- default will show icon by default if no icon is provided
+-- default shows no icon by default
   -- quit_on_open = 0,
   -- git_hl = 1,
   -- disable_window_picker = 0,
