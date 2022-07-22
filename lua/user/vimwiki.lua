c1de65c96f6871a73f9eb45a9da76bdf226dfa6c
Sven Hergenhahn <sven.hergenhahn@dm.de>
2022-07-22 10:26:57 +0200

    Migrate vimwiki autocmd to lua style

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -1,19 +1,30 @@
 
 local g = vim.g
-local cmd = vim.cmd
+
+local myhome = os.getenv("HOME")
+local mywiki = myhome .. '/vimwiki'
 
 g['vimwiki_global_ext'] = 0
 g['vimwiki_ext2syntax'] = {['.md']= 'markdown', ['.markdown']= 'markdown', ['.mdown']= 'markdown'}
-g['vimwiki_root'] = '~/vimwiki'
+g['vimwiki_root'] = mywiki
 g['vimwiki_listsyms'] = '✗✓'
 g['vimwiki_list'] = {
-    {path= '~/vimwiki/', syntax= 'markdown', ext= '.md'},
-    {path= '~/vimwiki/dm', syntax= 'markdown', ext= '.md'},
-    {path= '~/vimwiki/dm/cali', syntax= 'markdown', ext= '.md'},
-    {path= '~/vimwiki/dm/cali/refinements', syntax= 'markdown', ext= '.md'},
-    {path= '~/vimwiki/dm/TAV', syntax= 'markdown', ext= '.md'},
-    {path= '~/vimwiki/tech', syntax= 'markdown', ext= '.md'},
-    {path= '~/vimwiki/tech/python', syntax= 'markdown', ext= '.md'}
+    {path = mywiki, syntax= 'markdown', ext= '.md'},
+    {path = mywiki .. '/dm', syntax= 'markdown', ext= '.md'},
+    {path = mywiki .. '/dm/cali', syntax= 'markdown', ext= '.md'},
+    {path = mywiki .. '/dm/cali/refinements', syntax= 'markdown', ext= '.md'},
+    {path = mywiki .. '/dm/TAV', syntax= 'markdown', ext= '.md'},
+    {path = mywiki .. '/tech', syntax= 'markdown', ext= '.md'},
+    {path = mywiki .. '/tech/python', syntax= 'markdown', ext= '.md'}
 }
 
-cmd 'au BufWritePost ~/vimwiki/*.md call AutoCommit()'
+-- otherwise it wil steal ctrl-i
+vim.api.nvim_set_keymap('n', '<leader>wl', '<Plug>VimwikiNextLink', {})
+
+-- autocommit my wiki edits
+-- local cmd = vim.cmd
+-- cmd 'au BufWritePost ~/vimwiki/*.md call AutoCommit()'
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { mywiki .. "/*.md"},
+  callback = "g:AutoCommit"
+})
