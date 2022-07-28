88116bb9e4e42008fa7de308ec0e1dda02071bfc
Sven Hergenhahn <sven.hergenhahn@dm.de>
2022-07-28 14:56:30 +0200

    disable winbar tp prevent E351 ml_get

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -31,13 +31,13 @@ vim.cmd [[
 
 ]]
 
-if vim.version().major > 0 or vim.version().minor > 7 then
-  vim.api.nvim_create_autocmd(
-    { "CursorMoved", "CursorHold", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost", "TabClosed" },
-    {
-      callback = function()
-        require("user.winbar").get_winbar()
-      end,
-    }
-  )
-end
+-- if vim.version().major > 0 or vim.version().minor > 7 then
+--   vim.api.nvim_create_autocmd(
+--     { "CursorMoved", "CursorHold", "BufWinEnter", "BufFilePost", "InsertEnter", "BufWritePost", "TabClosed" },
+--     {
+--       callback = function()
+--         require("user.winbar").get_winbar()
+--       end,
+--     }
+--   )
+-- end
