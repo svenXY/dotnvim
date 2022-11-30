55290c8117e5745ce006f0e8064a4b49e26cbf6b
Sven Hergenhahn <sven.hergenhahn@dm.de>
2022-11-30 16:22:32 +0100

    Adds rust-analyzer to LSP

--- lua/user/lsp/settings/rust_analyzer.lua
+++ lua/user/lsp/settings/rust_analyzer.lua
@@ -2,7 +2,7 @@
 local opts = {
     tools = { -- rust-tools options
         autoSetHints = true,
-        hover_with_actions = true,
+        -- hover_with_actions = true,
         inlay_hints = {
             show_parameter_hints = false,
             parameter_hints_prefix = "",
