ecf3adcd0893915481d6ff6e8cf45333921c07cb
Sven Hergenhahn <sven.hergenhahn@dm.de>
2022-11-30 16:19:25 +0100

    adds prettier and autotag

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -90,6 +90,8 @@ return packer.startup(function(use)
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "MunifTanjim/prettier.nvim"
+  use "windwp/nvim-ts-autotag" -- closing of tags
 
   use "simrat39/rust-tools.nvim"
 
