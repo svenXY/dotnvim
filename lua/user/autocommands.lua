8460ece6e90fb1afd2a1672abdd1418ce3c515d1
Sven Hergenhahn <sven.hergenhahn@dm.de>
2023-01-23 11:59:41 +0100

    fix gq command for markdown (null-ls issue)

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -41,3 +41,10 @@ vim.cmd [[
 --     }
 --   )
 -- end
+
+-- Use internal formatting for bindings like gq. 
+ vim.api.nvim_create_autocmd('LspAttach', {
+ callback = function(args)
+  vim.bo[args.buf].formatexpr = nil
+  end
+ })
