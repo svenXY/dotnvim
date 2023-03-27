f520fab19612ac74ddb49449a7247dfbff4ddeac
Sven Hergenhahn <sven.hergenhahn@dm.de>
2023-03-27 15:57:35 +0200

    refactor lua split line functions

--- lua/user/svh-util.lua
+++ lua/user/svh-util.lua
@@ -9,10 +9,8 @@ local function file_exists(file)
   return f ~= nil
 end
 
-local function mysplit (inputstr, sep)
-  if sep == nil then
-          sep = "%s"
-  end
+local function linesplit (inputstr, sep)
+  sep = sep or ','
   local t={}
   for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
           table.insert(t, str:match( "^%s*(.-)%s*$" ))
@@ -20,23 +18,33 @@ local function mysplit (inputstr, sep)
   return t
 end
 
+local function expand_homedir(path)
+  local tilde = '~'
+  if path:sub(1, #tilde) == tilde then
+    path = os.getenv('HOME') .. path:sub(2, #path)
+  end
+  return path
+end
 -- get all lines from a file, returns an empty 
 -- list/table if the file does not exist
-function M.lines_from(file)
+-- splits at sep (default: comma)
+function M.lines_from(file, sep)
+  file = expand_homedir(file)
   if not file_exists(file) then return {} end
   local lines = {}
   local token = {}
   for line in io.lines(file) do
-    token = mysplit(line, ',')
+    token = linesplit(line, sep)
     table.insert(lines,token)
   end
   return lines
 end
 
-
 -- tests the functions above
 --[[ local file = os.getenv('HOME') .. '/snippets_private.txt' ]]
 --[[ local lines = M.lines_from(file) ]]
 --[[ print(inspect(lines)) ]]
+--[[ print(expand_homedir('~/snippets_private.txt')) ]]
+--[[ print(expand_homedir('/etc/snippets_private.txt')) ]]
 
 return M
