83c9501966512f5c923b9bf97c5091770ac1c6b0
Sven Hergenhahn <sven.hergenhahn@dm.de>
2023-04-06 08:26:08 +0200

    disable winbar for now

--- lua/plugins/winbar.lua
+++ lua/plugins/winbar.lua
@@ -1,11 +1,12 @@
-return {
-  --[[ { 'fgheng/winbar.nvim', ]]
-  { 'svenXY/winbar.nvim',
-  config = function()
-    require('winbar').setup({
-      enabled = true,
-      right_align = true,
-    })
-  end
-  }
-}
+return nil
+--[[ return { ]]
+--[[   --[[ { 'fgheng/winbar.nvim',  ]]
+--[[   { 'svenXY/winbar.nvim', ]]
+--[[   config = function() ]]
+--[[     require('winbar').setup({ ]]
+--[[       enabled = true, ]]
+--[[       right_align = true, ]]
+--[[     }) ]]
+--[[   end ]]
+--[[   } ]]
+--[[ } ]]
