83085c072a217443a78f375bbae0e2c581108790
Sven Hergenhahn <sven.hergenhahn@dm.de>
2023-04-21 11:53:20 +0200

    configure noice commands

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,6 +4,51 @@ return {
     config = function()
       require("noice").setup({
         -- add any options here
+        commands = {
+          all = {
+            -- options for the message history that you get with `:Noice`
+            view = "split",
+            opts = { enter = true, format = "details" },
+            filter = {},
+          },
+          history = {
+            -- options for the message history that you get with `:Noice`
+            view = "split",
+            opts = { enter = true, format = "details" },
+            filter = {
+              any = {
+                { event = "notify" },
+                { error = true },
+                { warning = true },
+                { event = "msg_show", kind = { "" } },
+                { event = "lsp", kind = "message" },
+              },
+            },
+          },
+          -- :Noice last
+          last = {
+            view = "popup",
+            opts = { enter = true, format = "details" },
+            filter = {
+              any = {
+                { event = "notify" },
+                { error = true },
+                { warning = true },
+                { event = "msg_show", kind = { "" } },
+                { event = "lsp", kind = "message" },
+              },
+            },
+            filter_opts = { count = 1 },
+          },
+          -- :Noice errors
+          errors = {
+            -- options for the message history that you get with `:Noice`
+            view = "popup",
+            opts = { enter = true, format = "details" },
+            filter = { error = true },
+            filter_opts = { reverse = true },
+          },
+        },
       })
     end,
     dependencies = {
