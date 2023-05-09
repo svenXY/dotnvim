351008e325084cbd559c47efcb66e1c2da360d95
Sven Hergenhahn <sven.hergenhahn@dm.de>
2023-05-09 10:39:55 +0200

    close manpages with q

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,7 +1,8 @@
 vim.cmd [[
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd FileType qf,help,lspinfo nnoremap <silent> <buffer> q :close<CR> 
+    autocmd FileType man nnoremap <silent> <buffer> q :quit<CR> 
     autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
